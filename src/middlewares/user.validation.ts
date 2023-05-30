a3fe6fc03ceb4afe493584b352e4a1d5eb86bbc9
harsh <harshgami0509@gmail.com>
2023-05-30 12:33:26 +0530

    password validation updated

--- src/middlewares/user.validation.ts
+++ src/middlewares/user.validation.ts
@@ -2,31 +2,31 @@ import joi from "joi";
 import passwordComplexity from "joi-password-complexity";
 
 const createDtoValidation = joi.object({
-     name: joi.string().alphanum().min(3).max(25).trim(true).required(),
-     email: joi.string().email().trim(true).required(),
-     password: passwordComplexity()
+    name: joi.string().alphanum().min(3).max(25).trim(true).required(),
+    email: joi.string().email().trim(true).required(),
+    password: passwordComplexity().required(),
 });
 
 const updateDtoValidation = joi.object({
     name: joi.string().alphanum().min(3).max(25).trim(true).required()
 });
 
-class UserValidator{
-    async create(req:any, res:any, next:any){
+class UserValidator {
+    async create(req: any, res: any, next: any) {
         const { error } = createDtoValidation.validate(req.body);
         if (error) {
             res.status(406);
-            return res.json({message:`Error in User Data : ${error.message}`});
+            return res.json({ message: `Error in User Data : ${error.message}` });
         } else {
             next();
         }
     };
-    
-    async update(req:any,res:any,next:any){
+
+    async update(req: any, res: any, next: any) {
         const { error } = updateDtoValidation.validate(req.body);
         if (error) {
             res.status(406);
-            return res.json({message:`Error in User Data : ${error.message}`});
+            return res.json({ message: `Error in User Data : ${error.message}` });
         } else {
             next();
         }
--- src/routes/user.route.ts
+++ src/routes/user.route.ts
@@ -4,10 +4,10 @@ import UserValidator from '../middlewares/user.validation';
 
 const router = Router();
 
-router.post('/users',UserValidator.create, UserController.createUser);
+router.post('/users', UserValidator.create, UserController.createUser);
 router.get('/users', UserController.getAllUsers);
 router.get('/users/:id', UserController.getUserById);
-router.put('/users/:id',UserValidator.update, UserController.updateUser);
+router.put('/users/:id', UserValidator.update, UserController.updateUser);
 router.delete('/users/:id', UserController.deleteUser);
 
 export default router;
